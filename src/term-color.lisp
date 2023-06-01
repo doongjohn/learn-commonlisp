0c6a77154de18f85040522d3e462a084c35d1f4b
doongjohn <doongjohn@gmail.com>
2023-06-01 22:18:29 +0900

    update

--- src/term-color.lisp
+++ src/term-color.lisp
@@ -1,22 +1,25 @@
-(defpackage term-color
+(defpackage #:term-color
   (:use
    #:cl)
   (:export
-   #:fg-set
-   #:fg-reset
-   #:fmt-fg))
+   #:fg-set   ; function
+   #:fg-reset ; function
+   #:fmt-fg   ; macro
+  ))
 
-(in-package term-color)
+(in-package #:term-color)
 
+;; https://en.wikipedia.org/wiki/ANSI_escape_code
+;; https://gist.github.com/JBlond/2fea43a3049b38287e5e9cefc87b2124
 (defconstant +colors+ '(:black 0
-                      :red 1
-                      :green 2
-                      :yellow 3
-                      :blue 4
-                      :purple 5
-                      :cyan 6
-                      :white 7
-                      :default 9))
+                        :red 1
+                        :green 2
+                        :yellow 3
+                        :blue 4
+                        :purple 5
+                        :cyan 6
+                        :white 7
+                        :default 9))
 
 (defun fg-set (color-name)
   (format t "~a[~am" #\Esc (+ 30 (getf +colors+ color-name))))
